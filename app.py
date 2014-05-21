5c0215546bfadf10db5e1d8d4ba4a0c685ab81d9
fhebert-perkins <fhebert-perkins16@bayschoolsf.org>
2014-05-21 09:34:53 -0700

    Updated app.py
    made the method in login work correctly, mysql with app.py

--- app.py
+++ app.py
@@ -1,27 +1,54 @@
 from flask import Flask, request, redirect, url_for, send_from_directory, session, render_template, Response
-import dbmysql
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                     user="resource_tracker", # your username
+                      passwd="password", # your password
+                      db="resource_tracker") # name of the data base
+cur = db.cursor()
 
 app = Flask(__name__)
 app.config.from_object('app.config')
+
 login_info = {'temptemp': 'temptemppassword', 'admin': 'adminPassword'}
+def get_history(id):
+	cur.excecute('SELECT * data WHERE id = %s', % id)
+	if len(cur.fetchall()) > 0:
+		toreturn = []
+		ids = []
+		date = []
+		type = []
+		data = []
+		for row in cur.fetchall():
+			ids.append(row[0])
+			date.append(row[1])
+			type.append(row[2])
+			data.append(row[3])
+		toreturn.append(ids)
+		toreturn.append(data)
+		toreturn.append(type)
+		toreturn.append(data)
+
+		return toreturn
+	else:
+		return False
+
 @app.route('/', methods=['GET', 'POST'])
 def root():
 	if session['logged_in']:
 		if request.method == 'post':
 			error = []
-			if request.form['resource_id'] in resource_ids: # form field called resource id
-				#get basic data about resource id, concatinate new info inject into db
-			else:
-				error.append('No such id, creating new id')
+			if 'resource_id' in request.form['resource_id']:
+				
 				#check if form is complete, if so create new entry in database, other wise error out
 			return #sucess + errors
 	return redirect(url_for('login'))
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if not session['logged_in']:
+	if not session['ldogged_in']:
 		if request.method == 'post':
 			try:
-				if  request.form['password']== login_info[request.form['username']]:
+				if  request.form['password'] == login_info[request.form['username']]:
 					session['logged_in'] = True
 					return redirect(url_for('root'))
 				return render_template('login.html', error='Incorect login info')
@@ -38,4 +65,5 @@ def getinfo:
 			return render_template('getinfo.html', info=info)
 		return render_template('getinfo.html')
 	return redirect(url_for('login'))
+
 
