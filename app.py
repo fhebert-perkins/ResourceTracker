106867e8d48d218bdf63edea85dbd2ddf4f974b8
fhebert-perkins <fhebert-perkins16@bayschoolsf.org>
2014-05-21 16:45:19 -0700

    IT WORKS

--- app.py
+++ app.py
@@ -4,22 +4,25 @@ import MySQLdb
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.update(dict(
+	USERNAME='admin',
+	PASSWORD='default'
 ))
 login_info = {'temptemp':'temptemppassword', 'admin':'password'}
 @app.route('/')
 def root():
 	return redirect(url_for('getinfo'))
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST','GET'])
 def login():
-	if not session.get('logged_in'):
-		if request.method == 'post':
-			#if request.form['password'] == login_info[request.form['username']]:
-			if request.form['password'] == 'password' and request.form['username'] == 'admin':
-				session['logged_in'] = True
-				return redirect(url_for('getinfo'))
-			return render_template('login.html', error=True)
-		return render_template('login.html')
-	return redirect(url_for('root'))
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid Username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Password'
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('getinfo'))
+	return render_template('login.html', error=error)
 @app.route('/getinfo', methods=['GET','POST'])
 def getinfo():
 	if session.get('logged_in'):
@@ -31,4 +34,4 @@ def getinfo():
 
 if __name__ == '__main__':
 	app.secret_key = 'as;ldfjkas;lfj2i1212'
-	app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0')
