18ca804d079bbc6205bb0220fc75474472eebe74
fhebert-perkins <fhebert-perkins16@bayschoolsf.org>
2014-05-22 10:37:50 -0700

    config update
    added config file, no longer works

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
 from flask import Flask, request, redirect, url_for, send_from_directory, session, render_template, Response
 import MySQLdb
+# db = MySQLdb.connect(host="localhost", # your host, usually localhost
+#                      user="resource_tracker", # your username
+#                       passwd="password", # your password
+#                       db="data")
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.update(dict(
-	USERNAME='admin',
-	PASSWORD='default'
-))
+app.config.from_object('app.config')
 login_info = {'temptemp':'temptemppassword', 'admin':'password'}
 @app.route('/')
 def root():
 	return redirect(url_for('getinfo'))
-@app.route('/login', methods=['POST','GET'])
-def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME']:
-			error = 'Invalid Username'
-		elif request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid Password'
-		else:
-			session['logged_in'] = True
-			return redirect(url_for('getinfo'))
-	return render_template('login.html', error=error)
 @app.route('/getinfo', methods=['GET','POST'])
 def getinfo():
 	if session.get('logged_in'):
@@ -30,6 +18,23 @@ def getinfo():
 			return render_template('getinfo.html', firstnameoption=firstnames, lastnameoption=lastnames, )
 		return render_template('getinfo.html')
 	return redirect(url_for('login'))
+@app.route('/login', methods=['POST','GET'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] in login_info:
+			if request.form['password'] == login_info[request.form['username']]:
+				session['logged_in'] = True
+				return redirect(url_for('getinfo'))
+			else:
+				error = 'Invalid Password'
+		else:
+			error = 'Invalid Username'	
+	return render_template('login.html', error=error)
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	return redirect(url_for('login'))
 if __name__ == '__main__':
-	app.secret_key = 'as;ldfjkas;lfj2i1212'
-	app.run(debug=True, host='0.0.0.0')
+	app.secret_key = app.config['app_secretKey']
+	app.run(debug=app.config['app_debug'], host='0.0.0.0', port=app.config['app_port'])
