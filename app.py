524f34a69dd58205e37effe99d51ce0ac405091e
finley <fhebert-perkins16@bayschoolsf.org>
2014-07-09 14:52:22 -0700

    test

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config.update(dict(
 	sql_password='password',
 	sql_db='Tracker',
 	adminpanelURI=''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890') for _ in range(10)),
-	SESSION_COOKIE_DOMAIN='localhost'
+	SESSION_COOKIE_DOMAIN='coleyarbrough.com'
 ))
 def select(query):
 	db = MySQLdb.connect(host=app.config['sql_host'], user=app.config['sql_user'], passwd=app.config['sql_password'], db=app.config['sql_db'])
