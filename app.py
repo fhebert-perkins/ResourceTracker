9640fe77458930d54726631aa1d2ad98bda931a4
Finley Hebert-Perkins <fhebert-perkins16@bayschoolsf.org>
2016-02-01 15:21:49 -0800

    basic permissions

--- app.py
+++ app.py
@@ -7,19 +7,40 @@ app = Flask(__name__) # initiates flask webapp
 db.init(app)
 bcrypt.init(app)
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("logged_in") is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not User.query.filter_by(email=session.get("email")).first().admin:
+            abort(405)
+        elif session.get("logged_in") is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/')
 def root():
     return redirect(url_for('addtrans')) # if / is served, redirects webbrowser to the add transaction page
 
 @app.route('/search')
+@login_required
 def search():
     return "NYI"
 
 @app.route('/history', methods=['GET','POST']) # request methods allowed Post and Get
+@login_required
 def history():
     return "NYI"
 
 @app.route('/addtrans', methods=['POST','GET']) # adds transactions to the Transactions database
+@login_required
 def addtrans():
     return "NYI"
 
@@ -29,14 +50,12 @@ def login():
 	if not session.get('logged_in'):
         if request.method == "POST":
             try:
-                user = User.query.filter_by(email=request.form["email"]).first()
-            except:
-                flash("Incorrect Username or Password")
-                return render_template('login.html')
-            if user.login(request.form["password"]):
-                session["logged_in"] = True
+                user = User.query.filter_by(email=request.form["email"]).first() # get user that is
+                yield user.login(request.form["password"])
+                session["logged_in"]= True
+                session["email"] = user.email
                 return redirect(url_for("addtrans"))
-            else:
+            except:
                 flash("Incorrect Username or Password")
                 return render_template('login.html')
         else:
@@ -44,6 +63,7 @@ def login():
     return redirect(url_for('addtrans'))
 
 @app.route('/logout') # This can stay the same
+@login_required
 def logout():
     if session.get('logged_in'):
         session.pop('logged_in', None)
@@ -51,14 +71,17 @@ def logout():
     return redirect(url_for('login'))
 
 @app.route('/newuser', methods=['GET', 'POST'])
+@admin_required
 def newuser():
     return "NYI"
 
 @app.route("/users")
+@login_required
 def listusers():
     return "NYI"
 
 @app.route('/admin', methods=['GET', 'POST'])
+@admin_required
 def admin():
     return "NYI"
 
